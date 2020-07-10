b09edd1f9d1b08846e91226d8dd5ce3347fb8464
meter CJY <854916013@qq.com>
2020-07-10 10:12:28 +0800

    2020年7月10日10:12:05 （cjy）
    1、路由懒加载优化
    2、注册、登录增加国家省市区自动补充
    3、处理选择国家省市区时一直显示选择国家问题

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,10 +1,21 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-09 09:59:18
+ * @LastEditTime: 2020-07-10 10:10:45
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \app-cn\config\dev.env.js
+ */
+
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  SHOP_API:'"http://47.115.5.76:8082"',   //ERP api
-  // SHOP_API:'"http://serviceapp.tospino.com"',   //ERP api
-  // SHOP_API:'"http://192.168.3.107:93"',   //ERP api
+    NODE_ENV: '"development"',
+    //   SHOP_API:'"http://47.115.5.76:8082"',   //ERP api
+    // SHOP_API:'"http://serviceapp.tospino.com"',   //ERP api
+    // SHOP_API:'"http://192.168.3.107:93"',   //ERP api
+    // SHOP_API: '"http://192.168.3.107:83"',   //ERP api 韦业斌
+    SHOP_API: '"http://192.168.3.8:83"',   //ERP api 周弘恩
 })
